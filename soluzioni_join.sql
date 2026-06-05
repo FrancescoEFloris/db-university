1f1187602df8c7711bf6e63b9291ebe5b19e59d4
Francesco Floris <francescofloris.ef@gmail.com>
2026-06-05 16:04:38 +0200

    join dati studenti, con corso e dipartimento

--- soluzioni_join.sql
+++ soluzioni_join.sql
@@ -31,9 +31,22 @@ ORDER BY `c`.`name` ASC;
 /*Selezionare tutti gli studenti con i dati relativi 
     al corso di laurea a cui sono iscritti e il relativo dipartimento, 
     in ordine alfabetico per cognome e nome*/
+SELECT  CONCAT(`s`.`surname`,
+		" ",
+		`s`.`name`) AS `student_name`,
+		`dg`.`name` AS `degree`,
+		`dp`.`name` AS `department`
+FROM `students` AS `s`
+	JOIN `degrees` AS `dg` 
+    ON `s`.`degree_id` = `dg`.`id`
+	JOIN `departments` AS `dp`
+    ON `dg`.`department_id` = `dp`.`id`
+ORDER BY `s`.`surname` ASC,
+		 `s`.`name` ASC;
 
 /*Selezionare tutti i corsi di laurea con i relativi 
     corsi e insegnanti*/
+
 
 /*Selezionare tutti i docenti che insegnano nel 
     Dipartimento di Matematica (54)*/
